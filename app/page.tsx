addb938681eca61159c19d0f16d72e0c15bbf683
Luksonaa_Dev <lukazhozhadze53@gmail.com>
2025-07-09 10:00:49 +0400

    start of project

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,40 @@
+"use client";
+
 import Image from "next/image";
+import car from "@/public/WhatsApp Image 2025-07-09 at 09.38.30_346ec32f.jpg";
+import car1 from "@/public/WhatsApp Image 2025-07-09 at 09.38.30_3494090a.jpg";
+import car2 from "@/public/WhatsApp Image 2025-07-09 at 09.38.30_85235142.jpg";
+import car3 from "@/public/WhatsApp Image 2025-07-09 at 09.38.31_d3cb194b.jpg";
+import car4 from "@/public/WhatsApp Image 2025-07-09 at 09.38.31_1ab5d18b.jpg";
+import car5 from "@/public/WhatsApp Image 2025-07-09 at 09.38.30_ea2d6fd9.jpg";
+
+
+const cars = [car1, car2, car3, car4, car5, car];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="bg-red-500 min-h-screen p-4">
+      <div className="flex flex-col items-center gap-3 mb-6">
+        <h1 className="text-3xl font-bold text-white">CAR RENT</h1>
+        <div className="flex flex-row gap-3">
+          <span className="text-xl text-white font-semibold">+995 577 522 599</span>
+          <span className="text-xl text-white font-semibold">+995 706 121 412</span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <h1 className="text-3xl text-white font-bold">АРЕНДА АВТОМОБИЛЯ</h1>
+      </div>
+      <div className="flex flex-wrap justify-center gap-4 lg:flex flex-row">
+        {cars.map((carImage, index) => (
+          <div key={index} className="">
+            <Image
+              src={carImage}
+              alt={`Car ${index + 1}`}
+              width={300}
+
+              className="rounded-lg shadow-lg object-cover"
+            />
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
