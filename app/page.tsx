a433c1661e02f519489d0586b4eff0c170b0c1c9
Luksonaa_Dev <lukazhozhadze53@gmail.com>
2025-07-09 10:10:12 +0400

    number function

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import Image from "next/image";
 import car from "@/public/WhatsApp Image 2025-07-09 at 09.38.30_346ec32f.jpg";
 import car1 from "@/public/WhatsApp Image 2025-07-09 at 09.38.30_3494090a.jpg";
-import car2 from "@/public/WhatsApp Image 2025-07-09 at 09.38.30_85235142.jpg";
-import car3 from "@/public/WhatsApp Image 2025-07-09 at 09.38.31_d3cb194b.jpg";
-import car4 from "@/public/WhatsApp Image 2025-07-09 at 09.38.31_1ab5d18b.jpg";
 import car5 from "@/public/WhatsApp Image 2025-07-09 at 09.38.30_ea2d6fd9.jpg";
+import car4 from "@/public/WhatsApp Image 2025-07-09 at 09.38.31_1ab5d18b.jpg";
+import car3 from "@/public/WhatsApp Image 2025-07-09 at 09.38.31_d3cb194b.jpg";
+import Image from "next/image";
 
 
-const cars = [car1, car2, car3, car4, car5, car];
+
+const cars = [car1, car3, car4, car5, car];
 
 export default function Home() {
   return (
@@ -17,9 +17,14 @@ export default function Home() {
       <div className="flex flex-col items-center gap-3 mb-6">
         <h1 className="text-3xl font-bold text-white">CAR RENT</h1>
         <div className="flex flex-row gap-3">
-          <span className="text-xl text-white font-semibold">+995 577 522 599</span>
-          <span className="text-xl text-white font-semibold">+995 706 121 412</span>
+          <a href="tel:+995577522599" className="text-xl text-white font-semibold hover:underline">
+            +995 577 522 599
+          </a>
+          <a href="tel:+995706121412" className="text-xl text-white font-semibold hover:underline">
+            +995 706 121 412
+          </a>
         </div>
+
         <h1 className="text-3xl text-white font-bold text-center">АРЕНДА АВТОМОБИЛЯ</h1>
       </div>
       <div className="flex flex-wrap justify-center gap-4 lg:flex flex-row">
